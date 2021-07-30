81cc5f3c306f6eea55af3488cab297277f273a43
Sasha Moiseenko <54060100+sasha-moiseenko@users.noreply.github.com>
2021-07-30 08:52:08 +0300

    Update 2.sh

--- script_2/2.sh
+++ script_2/2.sh
@@ -3,6 +3,6 @@
 DIR="./random/*"
 for i in {1..100}; do touch ./random/"random_text_$i.txt"; done
 for file in $DIR; do
-	echo $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1) > $file
+	echo $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 1000 | head -n 1) > $file
 done
 
